8f9c544a7b559a64c53fc4e973645dc2391aed6a
Denis Lozhkin <ssdenis00@yandex.ru>
2021-07-28 17:52:24 +0300

    refact: add list output

--- src/components/JokesItem/JokesItem.vue
+++ src/components/JokesItem/JokesItem.vue
@@ -1,9 +1,20 @@
 <template>
   <li class="item">
-    <p class="joke">Анекдот</p>
+    <p class="joke">{{ joke.title }}</p>
     <button type="button" arial-lable="like" class="like"></button>
   </li>
 </template>
+
+<script>
+export default {
+  props: {
+    joke: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
 
 <style scoped>
 @import url("./jokesItem.css");
--- src/components/JokesList/JokesList.vue
+++ src/components/JokesList/JokesList.vue
@@ -1,12 +1,13 @@
 <template>
   <ul class="items">
-    <JokesItem />
+    <JokesItem v-for="joke of jokes" :key="joke" v-bind:joke="joke" />
   </ul>
 </template>
 
 <script>
 import JokesItem from "@/components/JokesItem/JokesItem";
 export default {
+  props: ["jokes"],
   components: {
     JokesItem,
   },
